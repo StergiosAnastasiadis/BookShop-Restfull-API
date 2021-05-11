727d53d901aa160b813703c93942d77b0719fcbc
Stergios Anastasiadis <44000859+StergiosAnastasiadis@users.noreply.github.com>
2021-05-11 21:08:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes/routes");
-const booksSchema = require("./models/schema");
 
 const PORT = process.env.PORT || 5000;
 const app = express();
