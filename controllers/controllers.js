8a524dbb6f517b14918470845580c3dea56ee44f
StergiosAnastasiadis <Stelioslonx@outlook.com>
2023-10-13 21:41:13 +0300

    add error statusCode in controllers response

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,81 +1,112 @@
-const mongoose = require("mongoose");
-const express = require("express");
-const schema = require("../models/schema");
-const Book = mongoose.model('Book', schema);
-
-const app = express();
-
+const mongoose = require('mongoose')
+const express = require('express')
+const schema = require('../models/schema')
+const Book = mongoose.model('Book', schema)
 
 const getABook = (req, res) => {
-  Book.findOne({
-    _id: req.params.id
-  }, (err, foundBooks) => {
-    if (foundBooks) {
-      res.json(foundBooks)
-    } else {
-      res.status(404).send("No Book found with that id");
+  Book.findOne(
+    {
+      _id: req.params.id,
+    },
+    (err, foundBooks) => {
+      if (foundBooks) {
+        res
+          .status(200)
+          .json({ error: false, statusCode: 200, data: foundBooks })
+      } else {
+        res
+          .status(404)
+          .send({ error: true, statusCode: 404, message: '"Book not found"' })
+      }
     }
-  })
-};
+  )
+}
 
 const addABook = (req, res) => {
-
   const newBook = new Book({
     title: req.body.title,
     author: req.body.author,
-    price: req.body.price
-  });
+    price: req.body.price,
+  })
 
-  newBook.save(function(err) {
+  newBook.save(function (err) {
     if (!err) {
-      res.status(201).send("Successfully added a Book");
+      res.status(201).send({
+        error: false,
+        statusCode: 201,
+        data: 'Successfully added a Book',
+      })
     } else {
-      res.send(err);
+      res.send(err)
     }
-  });
+  })
 }
 
 const getAllBooks = (req, res) => {
   Book.find((err, foundBooks) => {
     if (!err) {
-      res.send(foundBooks);
+      res.send({ error: false, statusCode: 200, data: foundBooks })
     } else {
-      console.log(err);
+      console.log(err)
     }
   })
-};
+}
 
 const updateABook = (req, res) => {
-  Book.update({
-    _id: req.params.id
-  }, {
-    $set: req.body
-  }, (err) => {
-    if (!err) {
-      res.send("Successfully updated book.");
-    } else {
-      res.status(404).send("No books found mathing that id.");
+  Book.update(
+    {
+      _id: req.params.id,
+    },
+    {
+      $set: req.body,
+    },
+    (err) => {
+      if (!err) {
+        res.send({
+          error: false,
+          statusCode: 200,
+          message: 'Successfully updated book.',
+        })
+      } else {
+        res.status(404).send({
+          error: true,
+          statusCode: 404,
+          message: 'No books found mathing that id.',
+        })
+      }
     }
-  });
-};
+  )
+}
 
 const deleteABook = (req, res) => {
-  Book.deleteOne({
-      _id: req.params.id
+  Book.deleteOne(
+    {
+      _id: req.params.id,
     },
     (err, foundBook) => {
       if (foundBook) {
-        res.status(202).send("Successfully deleted book")
+        res.status(202).send({
+          error: false,
+          statusCode: 202,
+          message: 'Successfully deleted book',
+        })
       } else {
-        res.status(404).send("No books found mathing that id.");
+        res
+          .status(404)
+          .send({
+            error: error,
+            statusCode: 404,
+            message: 'No books found mathing that id.',
+          })
       }
-    })
-};
+    }
+  )
+}
 
 module.exports = {
   addABook,
   getAllBooks,
   getABook,
   updateABook,
-  deleteABook
-};
+  deleteABook,
+}
