236a94f5d549b747123113c8ee4f81c1d7ce3080
StergiosAnastasiadis <Stelioslonx@outlook.com>
2023-10-13 23:37:54 +0300

    add user Routes

--- services/router.js
+++ services/router.js
@@ -1,14 +1,9 @@
 import { Router } from 'express'
-// import userRoutes from '../routes/userRoutes'
-// import cvRoutes from '../routes/cvRoutes'
-// import emailRoutes from '../routes/emailRoutes'
-// import creditRoutes from '../routes/creditRoutes'
+// import userRoutes  from '../routes/userRoutes'
+import userRoutes from '../routes/userRoutes.js'
 
 const router = Router()
 
-// router.use('/api/users', userRoutes)
-// router.use('/api/cv', cvRoutes)
-// router.use('/api/email', emailRoutes)
-// router.use('/api/credits', creditRoutes)
+router.use('/api/users', userRoutes)
 
 export default router
