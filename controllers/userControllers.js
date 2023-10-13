64c60316fb1c01246a54b0e8e55860b4a9bd42da
StergiosAnastasiadis <Stelioslonx@outlook.com>
2023-10-13 23:51:20 +0300

    import asynchandler

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,4 +1,5 @@
-import User from '../models/userModel'
+import User from '../models/userModel.js'
+import asyncHandler from 'express-async-handler'
 
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body
