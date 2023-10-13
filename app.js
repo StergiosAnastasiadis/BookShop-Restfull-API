daf145ce60c87b72e2550ae4f7bfcf52ae44d0ac
StergiosAnastasiadis <Stelioslonx@outlook.com>
2023-10-13 23:34:09 +0300

    use new router on app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import 'dotenv/config'
 import express from 'express'
 import { connectDB } from './db/connect.js'
-import routes from './routes/routes.js'
+// import routes from './routes/routes.js'
+import routes from './services/router.js'
 
 const PORT = process.env.PORT || 5000
 const app = express()
@@ -9,8 +10,9 @@ const app = express()
 connectDB()
 
 app.use(express.json())
+app.use(routes)
+// routes(app)
 
-routes(app)
 
 app.listen(PORT, () => {
   console.log(`Sever is running on port ${PORT}...`)
---
+++ services/router.js
@@ -0,0 +1,14 @@
+import { Router } from 'express'
+// import userRoutes from '../routes/userRoutes'
+// import cvRoutes from '../routes/cvRoutes'
+// import emailRoutes from '../routes/emailRoutes'
+// import creditRoutes from '../routes/creditRoutes'
+
+const router = Router()
+
+// router.use('/api/users', userRoutes)
+// router.use('/api/cv', cvRoutes)
+// router.use('/api/email', emailRoutes)
+// router.use('/api/credits', creditRoutes)
+
+export default router
